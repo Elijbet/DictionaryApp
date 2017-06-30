0a118f9d80f56dc25afcc098fea8560c569250d5
Eliza Khachatryan <elijbet@gmail.com>
2017-06-29 19:14:16 -0700

    fix ups

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -129,8 +129,7 @@ class WordsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_word
-      # @word = Word.find(params[:id])
-      @word = Word.find_by_word(params[:id])
+      @word = Word.find(params[:id])
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def word_params
